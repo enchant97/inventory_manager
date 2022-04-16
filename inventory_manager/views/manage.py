24be0037417963135782642a6db288d7301a799b
Leo Spratt <enchant97@users.noreply.github.com>
2022-04-16 12:58:31 +0100

    add category creation

--- inventory_manager/views/manage.py
+++ inventory_manager/views/manage.py
@@ -1,4 +1,6 @@
-from quart import Blueprint, render_template
+from quart import Blueprint, render_template, redirect, url_for, request
+
+from ..database import models
 
 blueprint = Blueprint("manage", __name__, url_prefix="/manage")
 
@@ -6,3 +8,46 @@ blueprint = Blueprint("manage", __name__, url_prefix="/manage")
 @blueprint.get("/")
 async def get_index():
     return await render_template("manage/index.jinja")
+
+
+@blueprint.get("/categories/")
+async def get_categories_index():
+    roots_count = await models.Category.filter(parent=None).count()
+    total_count = await models.Category.all().count()
+
+    return await render_template(
+        "manage/categories/index.jinja",
+        roots_count=roots_count,
+        total_count=total_count,
+    )
+
+
+@blueprint.get("/categories/new")
+async def get_categories_new():
+    categories = await models.Category.all()
+
+    return await render_template(
+        "manage/categories/new.jinja",
+        categories=categories,
+    )
+
+
+@blueprint.post("/categories/new")
+async def post_categories_new():
+    form = await request.form
+
+    name = form["name"].strip().lower()
+    description = form.get("description")
+    if description == "":
+        description = None
+    parent_id = form.get("parent-id")
+    if parent_id == "":
+        parent_id = None
+
+    category = await models.Category.create(
+        name=name,
+        description=description,
+        parent_id=parent_id,
+    )
+
+    return redirect(url_for(".get_categories_index"))
