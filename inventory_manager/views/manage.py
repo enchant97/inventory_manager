553a7793a7734ad62813320c75d68a55fc3f505c
Leo Spratt <enchant97@users.noreply.github.com>
2022-04-16 14:13:23 +0100

    add location creation

--- inventory_manager/views/manage.py
+++ inventory_manager/views/manage.py
@@ -51,3 +51,46 @@ async def post_categories_new():
     )
 
     return redirect(url_for(".get_categories_index"))
+
+
+@blueprint.get("/locations/")
+async def get_locations_index():
+    roots_count = await models.Location.filter(parent=None).count()
+    total_count = await models.Location.all().count()
+
+    return await render_template(
+        "manage/locations/index.jinja",
+        roots_count=roots_count,
+        total_count=total_count,
+    )
+
+
+@blueprint.get("/locations/new")
+async def get_locations_new():
+    locations = await models.Location.all()
+
+    return await render_template(
+        "manage/locations/new.jinja",
+        locations=locations,
+    )
+
+
+@blueprint.post("/locations/new")
+async def post_locations_new():
+    form = await request.form
+
+    name = form["name"].strip().lower()
+    description = form.get("description")
+    if description == "":
+        description = None
+    parent_id = form.get("parent-id")
+    if parent_id == "":
+        parent_id = None
+
+    location = await models.Location.create(
+        name=name,
+        description=description,
+        parent_id=parent_id,
+    )
+
+    return redirect(url_for(".get_locations_index"))
